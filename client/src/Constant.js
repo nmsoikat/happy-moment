71e8fa2557b1eaca2324f80d74429300cb6a4152
nmsoikat <nursaykat@gmail.com>
2022-06-04 23:40:08 +0600

    patch

--- client/src/Constant.js
+++ client/src/Constant.js
@@ -1,4 +1,4 @@
 // export const REACT_APP_PUBLIC_FOLDER = 'http://localhost:8800/images/'
 // export const API_URL = 'http://localhost:8800'
-export const REACT_APP_PUBLIC_FOLDER = 'https://gentle-wind.herokuapp.comimages/'
+export const REACT_APP_PUBLIC_FOLDER = 'https://gentle-wind.herokuapp.com/images/'
 export const API_URL = 'https://gentle-wind.herokuapp.com'
